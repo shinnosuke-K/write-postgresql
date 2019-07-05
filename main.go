96bf0865ace26cd3f8231b8dc7b04ec372f8021e
shinnosuke-K <shin.050195@gmail.com>
2019-07-05 22:42:57 +0900

    split function
    Tableの追加，削除の機能を分割

--- main.go
+++ main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"log"
-	"wite-postgresql/prefectures"
+	"write-postgresql/prefectures"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
+
+func create(db *gorm.DB) {
+	// create Table
+	for _, pre := range prefectures.Prefectures {
+		db.Exec(`create table ` + pre + `(deviValue integer, schoolName text, course text, url text)`)
+	}
+}
+
+func deleteDB(db *gorm.DB) {
+	//delete Table
+	for _, pre := range prefectures.Prefectures {
+		//db.Exec(`drop table ` + pre)
+		db.DropTable(pre)
+	}
+}
 
 func main() {
 	db, err := gorm.Open("postgres", "user=prefectures password=pre dbname=prefectures sslmode=disable")
@@ -15,14 +30,8 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// create Table
-	for _, pre := range prefectures.Prefectures {
-		db.Exec(`create table ` + pre + `(deviValue integer, schoolName text, url text)`)
-	}
+	create(db)
 
-	// delete Table
-	//for _, pre := range prefectures.Prefectures {
-	//	db.Exec(`drop table ` + pre)
-	//}
+	deleteDB(db)
 
 }
