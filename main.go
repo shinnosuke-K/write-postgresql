ea7010163e09681d3260f1a32cc31e45e486d026
shinnosuke-K <shin.050195@gmail.com>
2019-07-10 15:17:36 +0900

    create function
    マイグレーションを行う関数を実装

--- main.go
+++ main.go
@@ -8,6 +8,18 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
+type info struct {
+	SchoolID   int
+	Deviation  int
+	SchoolName string
+	Course     string
+	URL        string
+}
+
+func (info *info) Migrate(db *gorm.DB) {
+	db.AutoMigrate(info)
+}
+
 func createTable(db *gorm.DB) {
 	// create Table
 	for _, pre := range prefectures.Prefectures {
@@ -29,15 +41,15 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	pre := info{}
 
-	//db.AutoMigrate(&pre)
+	pre := info{}
+	pre.Migrate(db)
 
 	//pre.SchoolName = "sdfafsad"
 	//db.Create(&pre)
 
-	db.First(&pre)
-	db.Delete(&pre)
+	//db.First(&pre)
+	//db.Delete(&pre)
 
 	//createTable(db)
 	//deleteDB(db)
