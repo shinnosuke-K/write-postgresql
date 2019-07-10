8f5eaada80588a97842c1d509b4130b3a7c92f00
shinnosuke-K <shin.050195@gmail.com>
2019-07-10 19:05:50 +0900

    create function
    csvファイルからDBへ書き込みを行う関数の実装

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
+	"io/ioutil"
 	"log"
+	"os"
+	"strings"
 	"write-postgresql/prefectures"
 
 	"github.com/jinzhu/gorm"
@@ -9,15 +13,51 @@ import (
 )
 
 type info struct {
-	SchoolID   int
-	Deviation  int
-	SchoolName string
-	Course     string
-	URL        string
+	SchoolID   int    `json sql:AUTO_INCREMENT`
+	Deviation  string `json:"deviation"`
+	SchoolName string `json:"school_name"`
+	Course     string `json:"course"`
+	URL        string `json:"url"`
+	Prefecture string `json:"prefecture"`
 }
 
 func (info *info) Migrate(db *gorm.DB) {
 	db.AutoMigrate(info)
+}
+
+func (info *info) WriteCSV(db *gorm.DB) {
+	count := 0
+	files, err := ioutil.ReadDir("csv-name-url/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		if fileName := file.Name(); fileName != ".DS_Store" {
+			csvFile, err := os.Open("csv-name-url/" + fileName)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			reader := csv.NewReader(csvFile)
+
+			for {
+				line, err := reader.Read()
+				if err != nil {
+					break
+				}
+
+				info.SchoolID = count
+				count++
+				info.Deviation = line[0]
+				info.SchoolName = line[1]
+				info.Course = line[2]
+				info.URL = line[3]
+				info.Prefecture = strings.Replace(fileName, ".csv", "", 1)
+
+				db.Create(&info)
+			}
+		}
+	}
 }
 
 func createTable(db *gorm.DB) {
@@ -42,14 +82,17 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Instantiate
 	pre := info{}
-	pre.Migrate(db)
 
-	//pre.SchoolName = "sdfafsad"
-	//db.Create(&pre)
+	// Write csv to DB
+	//pre.WriteCSV(db)
 
-	//db.First(&pre)
-	//db.Delete(&pre)
+	// Create Table
+	db.CreateTable(&pre)
+
+	// Delete Table
+	//db.DropTable(&pre)
 
 	//createTable(db)
 	//deleteDB(db)
